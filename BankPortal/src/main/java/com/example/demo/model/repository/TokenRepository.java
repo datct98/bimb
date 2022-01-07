4119af168e89598259a2ea7138fccc98e694dc78
datct15 <kiwisun1998@gmail.com>
2022-01-07 16:50:19 +0700

    test new branch

--- BankPortal/src/main/java/com/example/demo/model/repository/TokenRepository.java
+++ BankPortal/src/main/java/com/example/demo/model/repository/TokenRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TokenRepository extends JpaRepository<Token, Integer> {
     Token findByUserId(int id);
+    Token findByToken();
 }
